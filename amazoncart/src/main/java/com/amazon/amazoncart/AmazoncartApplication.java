feeac48eaa3a682f8177eb150b1f1959ab018fb9
Shiak Basha <shiak.basha@nagarro.com>
2023-11-12 11:53:18 +0530

    Initial commit

---
+++ amazoncart/src/main/java/com/amazon/amazoncart/AmazoncartApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.amazoncart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazoncartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazoncartApplication.class, args);
+	}
+
+}
